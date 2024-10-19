b2afaedcc591206617d18807f4d2528ebf346e92
Matthew Thomas <mthomas@ftc.gov>
2024-10-19 18:06:48 -0400

    Initial commit

---
+++ patterns/footer-default.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Title: Default footer
+ * Slug: ftcunion-stewart/footer-default
+ * Categories: footer
+ * Block Types: core/template-part/footer
+ */
+?>
+
+<!-- wp:spacer {"height":60} -->
+<div style="height:60px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group -->
+<div class="wp-block-group">
+	<!-- wp:paragraph {"align":"center","fontSize":"tiny"} -->
+	<p class="has-text-align-center has-small-font-size">
+		Chapter 344 of NTEU Representing FTC Staff
+	</p>
+	<!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":60} -->
+<div style="height:60px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
