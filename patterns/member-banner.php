52c1d75e2ac975ed6fd851dc8a9632cbfdf46942
mwt <matthew@mwt.me>
2025-12-13 08:37:31 -0500

    dont call text color 'text'

--- patterns/member-banner.php
+++ patterns/member-banner.php
@@ -9,8 +9,8 @@
 
 <!-- wp:group {"className":"ftcunion-member-banner","layout":{"type":"default"}} -->
 <div class="wp-block-group ftcunion-member-banner">
-	<!-- wp:paragraph {"align":"center","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}},"backgroundColor":"foreground","textColor":"background"} -->
-	<p class="has-text-align-center has-background-color has-foreground-background-color has-text-color has-background"
+	<!-- wp:paragraph {"align":"center","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}},"backgroundColor":"foreground-headers","textColor":"background"} -->
+	<p class="has-text-align-center has-background-color has-foreground-headers-background-color has-text-color has-background"
 		style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--30)">
 		<strong>Member-only resource: Do not share!</strong>
 	</p>
